49891b76f4d4ad49e00c6c38d89a340d5b87560f
geerdan <80449988+geerdan666@users.noreply.github.com>
2026-03-19 12:07:56 +0300

    Added homework to js lesson 31

---
+++ lesson_js_31/main.js
@@ -0,0 +1,66 @@
+// Задача 1.
+// Напишите функцию calculateFinalPrice, которая принимает базовую цену товара, процент скидки и налоговую ставку. Функция должна вычислять скидку, затем прибавлять налог и возвращать итоговую цену.
+
+function calculateFinalPrice(price, discount, tax) {
+  return (price - (price * discount) / 100) * (1 + tax);
+}
+
+// Пример работы:
+// console.log(calculateFinalPrice(100, 10, 0.2)); // 108
+// console.log(calculateFinalPrice(100, 10, 0)); // 90
+
+// Задача 2.
+// Напишите функцию checkAccess, которая принимает имя пользователя и пароль. Если имя пользователя равно "admin" и пароль равен "123456", функция должна возвращать строку "Доступ разрешен", иначе — "Доступ запрещен".
+
+function checkAccess(login, password) {
+  if (login === "admin" && password === "123456") {
+    return "Доступ разрешен";
+  } else {
+    return "Доступ запрещен";
+  }
+}
+
+// console.log(checkAccess('admin', '123456'))
+
+// Задача 3.
+// Напишите функцию getTimeOfDay, которая принимает текущее время (число от 0 до 23) и возвращает строку:
+// "Ночь" (с 0 до 5 часов),
+// "Утро" (с 6 до 11 часов),
+// "День" (с 12 до 17 часов),
+// "Вечер" (с 18 до 23 часов).
+// Если введённое значение не попадает в этот диапазон, возвращайте `"Некорректное время"`.
+
+function getTimeOfDay(dayTime) {
+  if (dayTime > 23 || dayTime < 0) {
+    return "Некорректное время";
+  }
+
+  if (dayTime <= 5) {
+    return "Ночь";
+  } else if (dayTime <= 11) {
+    return "Утро";
+  } else if (dayTime <= 17) {
+    return "День";
+  } else {
+    return "Вечер";
+  }
+}
+
+// console.log(getTimeOfDay(18))
+
+// Задача 4.
+// Напишите функцию findFirstEven, которая принимает два числа start и end и находит первое чётное число в указанном диапазоне.
+// Если чётного числа в этом диапазоне нет, функция должна вернуть "Чётных чисел нет".
+
+function findFirstEven(start, end) {
+  for (i = start; i <= end; i++) {
+    if (i % 2 === 0) {
+      return i;
+    }
+  }
+  return "Чётных чисел нет"
+}
+
+// Пример работы:
+// console.log(findFirstEven(1, 10)); // 2
+// console.log(findFirstEven(9, 9)); // "Чётных чисел нет"
